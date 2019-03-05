f868fced37df0570bb07039b67fbcfadad4abe84
richietam <richardtam.ny@gmail.com>
2019-03-04 23:53:09 -0500

    finished lab

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,15 +2,29 @@ class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
+  def select_songs
+    Song.where("genre_id =#{self.id}")
+  end
+
   def song_count
     # return the number of songs in a genre
+    # Song.count("genre=#{self}")
+
+
+    Song.where(genre_id:Genre.find(self.id)).count
+
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    Song.where(genre_id:Genre.find(self.id)).select('DISTINCT artist_id').count
+
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    Song.where("genre_id =#{self.id}").map do |song|
+      song.artist.name
+    end.uniq
   end
+
+
 end
